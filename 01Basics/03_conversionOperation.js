6cf2b10e61563fb0ae4bea93598c52e15751ccb3
Raj Vradhan <rajvardhansingh120@gmail.com>
2025-06-12 13:03:29 +0000

    conversion of data types

---
+++ 01Basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isLogedIn = 1
+
+let booleanIsLogedIn = Boolean(isLogedIn)
+
+console.log(booleanIsLogedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "raj" => true
+
+let someNumber = 33
+
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
