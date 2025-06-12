632a86c0fe87952f8c295f65324e7d00abd785a2
Raj Vradhan <rajvardhansingh120@gmail.com>
2025-06-12 15:31:19 +0000

    Strings

---
+++ 01Basics/05_String.js
@@ -0,0 +1,34 @@
+const name = "Raj"
+const repoCount = 10
+
+// console.log(name + repoCount + " value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String(`Raj Vardhan`); // key value pairs
+
+console.log(gameName[0])
+console.log(gameName[1])
+console.log(gameName[2])
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(6));
+console.log(gameName.indexOf('V'));
+
+const newString = gameName.substring(0, 6);
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4);
+console.log(anotherString);
+
+const newStrOne = "   Raj Vardhan   "
+console.log(newStrOne);
+console.log(newStrOne.trim());
+
+const url = "https://raj.com/home%20ai"
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('raj'))
+
+console.log(gameName.split(' '))
