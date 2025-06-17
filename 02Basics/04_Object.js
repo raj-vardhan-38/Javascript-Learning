ce43582ab5c121b2f000ea2c42d3ae399121e51e
Raj Vradhan <rajvardhansingh120@gmail.com>
2025-06-17 12:41:54 +0000

    object

--- 02Basics/04_Object.js
+++ 02Basics/04_Object.js
@@ -7,5 +7,47 @@ tinderUser.isLoggedIn = false;
 // console.log(tinderUser);
 
 const regularUser = {
-    
+    email: "rajvardhan@gmail.com",
+    fullname: {
+        userfullname: {
+            firstName: "Raj",
+            LastName: "Vardhan"
+        }
+    }
 }
+
+console.log(regularUser.fullname.userfullname.firstName);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {obj1, obj2}
+
+// const obj3 = Object.assign({}, obj1, obj2)  // (target = {}, source)
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "rajvardhan@gmail.com"
+    },
+    {
+        id: 1,
+        email: "rajvardhan@gmail.com"
+    },
+    {
+        id: 1,
+        email: "rajvardhan@gmail.com"
+    },
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));  //output in array
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser)); // key:values in array
+
+console.log(tinderUser.hasOwnProperty('isLogged'));
