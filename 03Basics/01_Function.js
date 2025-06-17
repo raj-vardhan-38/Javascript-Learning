0373ded62fd5ebf4c195217ffdad4262817951ea
Raj Vradhan <rajvardhansingh120@gmail.com>
2025-06-17 13:45:34 +0000

    Function

--- 03Basics/01_Function.js
+++ 03Basics/01_Function.js
@@ -29,3 +29,34 @@ function loginUserMessage(username){
 }
 
 console.log(loginUserMessage("Raj"))
+
+function calcculateCartPrice(value1, value2, ...num1){
+    return num1
+}
+
+console.log(calcculateCartPrice(200, 400, 500, 2000));
+
+const User = {
+    username: "Raj",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(User)
+handleObject({
+    username: "Sam",
+    price: 399
+})
+
+const myArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myArray));
+
+console.log(returnSecondValue([200, 400, 100, 600]));
