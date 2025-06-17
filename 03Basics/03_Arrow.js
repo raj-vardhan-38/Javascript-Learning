acff0480e8563bccf26df5aef47365baf4b6676d
Raj Vradhan <rajvardhansingh120@gmail.com>
2025-06-17 15:19:22 +0000

    Arrow

---
+++ 03Basics/03_Arrow.js
@@ -0,0 +1,48 @@
+const user = {
+    username: "Raj",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username} , Welcome to website `);
+        console.log(this);
+        
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "Raj"
+//     console.log((this.username)); //=> undefined 
+// }
+// chai()
+
+
+// const chai = function(){
+//     let username = "Raj"
+//     console.log((this.username));
+// }
+// chai()
+
+
+// +++++++++++++ Arrow Function +++++++++++++
+const chai = () => {
+    let username = "Raj"
+    console.log((this));
+}
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) => num1 + num2
+
+// const addTwo = (num1, num2) => (num1 + num2)
+
+const addTwo = (num1, num2) => ({username: "Sam"})
+console.log(addTwo(3, 4));
