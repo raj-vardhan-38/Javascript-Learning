3fb5d46e6ee1bb4941b16e23a87cb0f361580353
Raj Vradhan <rajvardhansingh120@gmail.com>
2025-06-17 12:41:27 +0000

    Updated Object

---
+++ 02Basics/04_Object.js
@@ -0,0 +1,11 @@
+// const tinderUser = new Object()  // singleton obj
+const tinderUser = {}  // non singleton obj
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false;
+// console.log(tinderUser);
+
+const regularUser = {
+    
+}
